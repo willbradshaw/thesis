9cd7b973cdf44b2dc30041d2c5ae281037f2921d
Will Bradshaw <wjbradshaw1@gmail.com>
2019-04-03 12:20:14 +0200

    Various minor corrections

--- _Figures/scripts/ageing-igor-entropies.R
+++ _Figures/scripts/ageing-igor-entropies.R
@@ -127,7 +127,3 @@ g_h_group <- plot_entropy_table(h_group) +
              labeller = as_labeller(function(c) paste0("Age (days) = ", c))) +
   theme(strip.text.x = element_text(hjust = 0.9))
 savefig(g_h_group, filename_base, height = 20, width = 28)
-
-#------------------------------------------------------------------------------
-# COMPARE BETWEEN AGE GROUPS
-#------------------------------------------------------------------------------
